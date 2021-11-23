0b03860faf7f6dfd94ea5b250b509750bed462c4
Benjamin <dotsehyde@gmail.com>
2021-11-23 15:24:31 +0000

    data & models

---
+++ src/app/data/data.ts
@@ -0,0 +1,27 @@
+import { Task } from './Task';
+
+export const TASK: Array<Task> = [{
+  id: 1,
+  text: "Hello task 1",
+  day: "May 5 at 2:00pm",
+  remainder:true,
+
+},{
+  id: 2,
+  text: "Hello task 2",
+  day: "May 14 at 3:50pm",
+  remainder:false,
+
+},{
+  id: 3,
+  text: "Hello task 2",
+  day: "May 20 at 5:00pm",
+  remainder:true,
+  },
+  {
+    id: 4,
+    text: "Going to School",
+    day: "May 23 at 5:30pm",
+    remainder:false,
+  },
+]
